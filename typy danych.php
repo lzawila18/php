4047b37c7fbaa167046997f9ed188915f4b9662c
lzawila18 <lzawila254@gmail.com>
2025-09-05 15:24:05 +0200

    Dodano skrypt

---
+++ typy danych.php
@@ -0,0 +1,10 @@
+<?php
+$a_bool = TRUE;
+$name = 'Karol';
+$an_int = 12;
+$pi = 3.31415926;
+echo gettype($a_bool) . "<br>";
+echo is_float($an_int) . "<br>";
+print var_dump($name); . "<br>";
+
+?>
