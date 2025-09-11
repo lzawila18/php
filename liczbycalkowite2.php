8f6a1fa33e4889f0796c445751fd681b37384d82
lzawila18 <lzawila254@gmail.com>
2025-09-11 08:37:42 +0200

    Dodano skrypt

---
+++ liczbycalkowite2.php
@@ -0,0 +1,12 @@
+<?php
+$Liczba1 =42;
+print ("Liczba to: " . $liczba1;)
+$LiczbaOctalna = 042;
+print ("Liczba ósemkowa to: " . $liczbaOctalna;);
+$LiczbaHex = 0x2A;
+print ("Liczba szesnastkowa to: " . $liczbaHex);
+$LiczbaBinarna = 0b101010;  
+print ("Liczba binarna to: " . $liczbaBinarna);
+$LiczbaDuza = 1_234_567;
+print ("Duża liczba to: " . $liczbaDuza);
+?>
