4046f1054ae1f9c472a68671b1c12ad9bb59bb76
lzawila18 <lzawila254@gmail.com>
2025-09-11 09:47:51 +0200

    `Added string manipulation examples and functions usage`

--- stringi.php
+++ stringi.php
@@ -5,4 +5,31 @@ echo "You deleted C:\\Windows\\System32<br>";
 
 //stringi w cudzyslowach interpretują znaki modyfikacji
 echo "To jest pierwszy wiersz\nTo jest drugi wiersz<br>";
+
+//stringi w apostrofach nie interpretują znaków modyfikacji
+echo 'To jest pierwszy wiersz\nTo jest drugi wiersz<br>';
+
+echo <<<END
+        a
+      b
+    c
+\n<br>
+END;
+echo <<<END
+        a
+      b
+    c
+<br>
+END;
+//Nowdoc generuje sformatowany tekst bez interpretacji znaków modyfikacji
+echo <<<'EOD'
+        a
+      b
+    c
+<br>
+EOD;
+//strlen, mb_strlen - długość stringa i liczba znaków
+echo strlen("Ala ma kota") . "<br>";
+echo mb_strlen("Ala ma kota<br>") . "<br>";
+  
 ?>
