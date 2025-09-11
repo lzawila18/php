fe88fb618abed1482df1b50d246ea34aa632836b
lzawila18 <lzawila254@gmail.com>
2025-09-11 10:35:39 +0200

    Aktualizowano skrypt

--- stringi.php
+++ stringi.php
@@ -29,7 +29,41 @@ echo <<<'EOD'
 <br>
 EOD;
 //strlen, mb_strlen - długość stringa i liczba znaków
-echo strlen("Ala ma kota") . "<br>";
-echo mb_strlen("Ala ma kota<br>") . "<br>";
+echo strlen("Grzegżółka");
+echo "<br>";
+echo mb_strlen("Grzegżółka");
+echo "<br>";
+
+//str_replace - zamiana fragmentu stringa
+echo str_replace("Grzegżółka", "Kowalski", "Pan Grzegżółka to kolega");
+echo "<br>";
+
+//strpos - pozycja pierwszego wystąpienia podanego fragmentu stringa
+echo strpos("Pan Kowalski to kolega", "Kowalski");
+echo "<br>";
+
+//funkcja print sluzy do generowania sformatowanego tekstu
+$tekst = "Hello";
+printf("[%s]\n", $tekst); // wypisze [Hello]
+printf("[%10s]\n", $tekst); // wypisze [     Hello]
+printf("[%-10s]\n", $tekst); // wypisze [Hello     ]  
+printf("[%.3s]\n", $tekst); // wypisze [Hel]
+printf("[%10.3s]\n", $tekst); // wypisze [       Hel
+printf("[%-10.3s]\n", $tekst); // wypisze [Hel       ]
+echo "<br>";
   
+$n = 43951789;
+printf("%%d = '%d'\n", $n);
+echo "<br>";
+printf("%%f = '%f'\n", $n);
+echo "<br>";
+printf("%%e = '%e'\n", $n);
+echo "<br>";
+printf("%%b = '%b'\n", $n);
+echo "<br>";
+printf("%%o = '%o'\n", $n);
+echo "<br>";
+printf("%%s = '%s'\n", $n);
+echo "<br>";
+printf("%%x = '%x'\n", $n);
 ?>
