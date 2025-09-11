3b24d8bb31023fe2e79c936ef9432b35714be6ea
lzawila18 <lzawila254@gmail.com>
2025-09-11 08:49:58 +0200

    Nowy

--- liczbyrzeczywiste.php
+++ liczbyrzeczywiste.php
@@ -1,9 +1,9 @@
 <?php
 $a = 1.234;
-print "liczba a = $a\n";
+print "liczba a = $a<br>";
 $b = 1.2e3;
-print "liczba b = $b\n";
+print "liczba b = $b<br>";
 $c = 7E-10;     
-print "liczba c = $c\n";
+print "liczba c = $c<br>";
 $d = 1_234.567;
-print "liczba d = $d\n";
+print "liczba d = $d<br>";
