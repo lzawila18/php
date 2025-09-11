fd14896c59b6d3e979d8e3f26d194830f34fd6a1
lzawila18 <lzawila254@gmail.com>
2025-09-11 09:19:11 +0200

    Test

--- stringi.php
+++ stringi.php
@@ -2,4 +2,7 @@
 echo 'Arnold once said: "I\'ll be back"<br>';
 
 echo "You deleted C:\\Windows\\System32<br>";
+
+//stringi w cudzyslowach interpretują znaki modyfikacji
+echo "To jest pierwszy wiersz\nTo jest drugi wiersz<br>";
 ?>
