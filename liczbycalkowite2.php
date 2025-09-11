0b035b71965086f2d9bb8dbb2a68f4bea772e4d5
lzawila18 <lzawila254@gmail.com>
2025-09-11 08:39:14 +0200

    Naprawiono

--- liczbycalkowite2.php
+++ liczbycalkowite2.php
@@ -1,8 +1,8 @@
 <?php
 $Liczba1 =42;
-print ("Liczba to: " . $liczba1;)
+print ("Liczba to: " . $liczba1);
 $LiczbaOctalna = 042;
-print ("Liczba ósemkowa to: " . $liczbaOctalna;);
+print ("Liczba ósemkowa to: " . $liczbaOctalna);
 $LiczbaHex = 0x2A;
 print ("Liczba szesnastkowa to: " . $liczbaHex);
 $LiczbaBinarna = 0b101010;  
